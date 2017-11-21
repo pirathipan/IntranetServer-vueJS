71a67d9d961a331e8557f3f7ed90b92a20d9b5e3
pirathipan <pirathipan.rupert-diknes@supinternet.fr>
2017-11-21 11:43:52 +0100

    add intranet server

---
+++ Collaborateur.controller.js
@@ -0,0 +1,93 @@
+// Inclusion du modèle permettant de manipuler en base des collaborateurs
+const Collaborateurs = require('./Collaborateur.model')
+
+
+module.exports = {
+   /*
+    GET /collaborateurs
+    Renvoie la liste de tous les collaborateurs en base
+    */
+  findAll : function(req,res){
+      Collaborateurs.find({})
+          .exec()
+          .then(function(collaborateur){
+              if(collaborateur === null){
+                  return res.status(500).json({error:1, message:'Aucun collaborateur trouvé'})
+              }
+              res.json(collaborateur);
+          })
+          .catch(
+              function(err){
+                  res.status(500).json({error:1, message:err.message})
+              });
+  },
+    /*
+     GET /collaborateur/;id
+     Recherche un collaborateur par son id
+     */
+    findOne: function(req,res){
+      let id = req.params.id;
+        Collaborateurs.findById({_id: id})
+            .exec()
+            .then(function(collaborateur){
+                if(collaborateur === null){
+                    return res.status(500).json({error:1, message:`Collaborateur _id ${id} non trouvé`})
+                }
+
+                res.json(collaborateur);
+            })
+            .catch(
+            function(err){
+                res.status(500).json({error:1, message:err.message})
+            });
+    },
+    /*
+     POST /collaborateur
+     Ajoute un collaborateur
+     */
+    create: function(req,res){
+        Collaborateurs
+            .create(req.body)
+            .then(function(collaborateur){
+                res.json({success:1, message: 'Collaborateur créé', inserted: collaborateur})
+            })
+            .catch(function(err){
+                res.status(500).json({error:1, message:err.message})
+            })
+    },
+    /*
+     PUT /collaborateur/:id
+     Edit une collaborateur possendant l'id :id
+     */
+    update: function(req,res){
+        let id = req.params.id;
+        let data = req.body;
+       Collaborateurs
+           .findByIdAndUpdate(id, data, {new:true}) //par defaut id,data renvoit l'utilisateur avant l'update le 3 param true renvoit l'user apres modif
+           .exec()
+           .then(function(updatedUser){
+               res.json({success:1, message: 'Collaborateur updated', updated: updatedUser})
+
+           })
+           .catch(function(err){
+               res.status(500).json({error:1, message:err.message})
+           })
+    },
+    /*
+     DELETE /collaborateur/:id
+     Delete un collaborateur
+     */
+    remove: function(req,res){
+        let id = req.params.id;
+        Collaborateurs
+            .findByIdAndRemove(id)
+            .exec()
+            .then(function(deletedCollaborateur){
+                res.json({success:1, message:'Collaborateur supprimée', deleted: deletedCollaborateur})
+            })
+            .catch(function(err){
+                res.status(500).json({error:1, message:err.message})
+            })
+    },
+
+};
---
+++ index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+//use nvm pour switch de node version
+//nvm list
+//nvm use <version>
+
+//nvm install <node_version>
+
+//node -v
+
+require('colors'); //permet d'équiper le type String de toutes plein de méthode (qui permet le display de color)
+
+const util = require('util');
+const bodyParser = require('body-parser');
+
+
+//on peut promise des méthode avec util.promisify()
+//exemple
+//const readFile = util.promisify(fs.readFile) //on peut promissiy tous
+// readFile('filename.txt').then(data => {data.toString()}).catch(console.error)
+
+//async / await
+//async function appStart() { const fileContent = await readFile('filename.txt) }
+
+//les deux font la m^mee chose et permette de "blocker un traitement en attendant une reponse (une promesse)" sans blocker le processus node (dispo depuis v8)
+
+const express = require('express');
+
+//Creation d'une appliccation expressJS
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//config header
+app.use('/', (req,res,next) => {
+    //on intranet (client) else *
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
+
+const mongoose = require('mongoose');
+
+const Collaborateur = require('./Collaborateur.controller');
+
+
+app.get('/collaborateurs', Collaborateur.findAll);
+
+
+app.get('/collaborateur/:id', Collaborateur.findOne);
+
+app.post('/collaborateur', Collaborateur.create);
+
+app.put('/collaborateur/:id',Collaborateur.update );
+
+app.delete('/collaborateur/:id', Collaborateur.remove);
+
+// ou
+/*
+ app.get('/collaborateurs', CollbaorateurCtrl.findAll)
+ */
+
+
+
+
+//config via express (creer des clef)
+app.set('ip', 'localhost');
+app.set('port', 1338);
+
+app.set('db_name', 'intranet');
+
+// demare la db au dermarage du server
+//indique a mongoose qu'on utilise les promesses à utiliser sont celle par défaut dans Node.js(objet global)
+mongoose.Promise = global.Promise //permet les operations chainée (.exec puis .then);
+
+// Transformation de la méthode app.listen() d'Express en "Promesse JS"
+const appListen = (app, port, ip) => {
+    return new Promise((resolve, reject) => {
+        app.listen(port, ip, resolve)
+    })
+};
+
+// Connexion à la base de données MONGO,
+
+//attention promissify ici et codé par nous même, car les function qui se promisiffy fonctionne SEULEMENT si elle sont de type retyour err,data et non de retour callback
+//donc uniquement les method convionné node (err,data) en retour se promissify sinon on le fait à la main
+//ainsi listen venat de express ne peut pas etre promissify car il return une err ou une callback
+
+mongoose
+    .connect('mongodb://localhost:27017/intranet', {useMongoClient:true})
+    .then( () => console.log('MongoDB : Connexion établie'.bgGreen) )
+    .then( appListen(app, app.get('port'), app.get('ip')) )
+    .then( () => console.log(` App Started on http://${app.get('ip')}:${app.get('port')} `.bgGreen) )
+    .catch(err => console.log(err.message.red))
